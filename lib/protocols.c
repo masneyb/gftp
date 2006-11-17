300f1f484a62239f491d179086916cd2fb79e98c
Brian Masney <masneyb@gftp.org>
2006-11-17 01:20:58 +0000

    lib/protocols.c - skip over the hidden files when doing a recursive
    2006-11-16 Brian Masney <masneyb@gftp.org>
    
            * lib/protocols.c - skip over the hidden files when doing a recursive
            transfer if the shown_hidden_files option is disabled. (closes #321573)

--- lib/protocols.c
+++ lib/protocols.c
@@ -1861,31 +1861,37 @@ gftp_parse_ls (gftp_request * request, const char *lsoutput, gftp_file * fle,
 static GHashTable *
 gftp_gen_dir_hash (gftp_request * request, int *ret)
 {
+  intptr_t show_hidden_files;
   GHashTable * dirhash;
   gftp_file * fle;
   off_t *newsize;
 
-  dirhash = g_hash_table_new (string_hash_function, string_hash_compare);
   *ret = gftp_list_files (request);
-  if (*ret == 0)
+  if (*ret != 0)
+    return (NULL);
+      
+  dirhash = g_hash_table_new (string_hash_function, string_hash_compare);
+  gftp_lookup_request_option (request, "show_hidden_files", &show_hidden_files);
+
+  fle = g_malloc0 (sizeof (*fle));
+  while (gftp_get_next_file (request, NULL, fle) > 0)
     {
-      fle = g_malloc0 (sizeof (*fle));
-      while (gftp_get_next_file (request, NULL, fle) > 0)
+      if (!show_hidden_files && *fle->file == '.' &&
+          strcmp (fle->file, "..") != 0)
         {
-          newsize = g_malloc (sizeof (*newsize));
-          *newsize = fle->size;
-          g_hash_table_insert (dirhash, fle->file, newsize);
-          fle->file = NULL;
           gftp_file_destroy (fle, 0);
+          continue;
         }
-      gftp_end_transfer (request);
-      g_free (fle);
+
+      newsize = g_malloc (sizeof (*newsize));
+      *newsize = fle->size;
+      g_hash_table_insert (dirhash, fle->file, newsize);
+      fle->file = NULL;
+      gftp_file_destroy (fle, 0);
     }
-  else
-    {
-      g_hash_table_destroy (dirhash);
-      dirhash = NULL;
-    }
+
+  gftp_end_transfer (request);
+  g_free (fle);
 
   return (dirhash);
 }
@@ -1914,11 +1920,15 @@ gftp_destroy_dir_hash (GHashTable * dirhash)
 static GList *
 gftp_get_dir_listing (gftp_transfer * transfer, int getothdir, int *ret)
 {
+  intptr_t show_hidden_files;
   GHashTable * dirhash;
   GList * templist;
   gftp_file * fle;
   off_t *newsize;
   char *newname;
+
+  gftp_lookup_request_option (transfer->fromreq, "show_hidden_files",
+                              &show_hidden_files);
 
   if (getothdir && transfer->toreq != NULL)
     {
@@ -1940,7 +1950,8 @@ gftp_get_dir_listing (gftp_transfer * transfer, int getothdir, int *ret)
   templist = NULL;
   while (gftp_get_next_file (transfer->fromreq, NULL, fle) > 0)
     {
-      if (strcmp (fle->file, ".") == 0 || strcmp (fle->file, "..") == 0)
+      if (strcmp (fle->file, ".") == 0 || strcmp (fle->file, "..") == 0 ||
+          (!show_hidden_files && *fle->file == '.'))
         {
           gftp_file_destroy (fle, 0);
           continue;
