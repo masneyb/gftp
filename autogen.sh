f823d26f492f43a3dc87250c75c1412ba95fb7b2
wdlkmpx <wdlkmpx@gmail.com>
2020-01-23 04:51:27 +0800

    update build system
    delete and replace really old stuff with newer stuff
    
    this is a big update and everything is a bit faster
    
    autogen.sh no longer calls ./configure
    
    To compile:
    ./autogen.sh
    ./configure --prefix=/usr
    make
    make install
    
    I tested this change in 2 systems:
    
    * debian stretch (2017)    - SUCCESS
    * slackware 14.0 (2012)    - SUCCESS
    
    So I think this change doesn't introduce bugs and is pretty generic..

--- autogen.sh
+++ autogen.sh
@@ -1,100 +1,35 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
-# This was derived from Glib's autogen.sh
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
-
-ORIGDIR=`pwd`
 cd $srcdir
+
 PROJECT=gFTP
-TEST_TYPE=-f
-FILE=lib/gftp.h
-GETTEXTIZE=gettextize
-
-DIE=0
-
-AUTOMAKE=automake
-ACLOCAL=aclocal
-AUTOCONF=autoconf
-AUTOHEADER=autoheader
-
-autoconf_version=`$AUTOCONF --version 2>/dev/null`
-if [ "x$autoconf_version" = "x" ] ; then 
-       echo
-       echo "GNU autoconf must be installed to build $PROJECT"
-       echo "GNU autoconf is available from http://www.gnu.org/software/autoconf/"
-       DIE=1
-fi
-
-automake_version=`$AUTOMAKE --version 2>/dev/null`
-if [ "x$automake_version" = "x" ] ; then 
-       echo
-       echo "GNU automake must be installed to build $PROJECT"
-       echo "GNU automake is available from http://www.gnu.org/software/automake/"
-       DIE=1
-fi
-
-gettext_version=`$GETTEXTIZE --version 2>/dev/null | grep 'GNU'`
-if [ "x$gettext_version" = "x" ] ; then 
-       echo
-       echo "GNU gettext must be installed to build $PROJECT"
-       echo "GNU gettext is available from http://www.gnu.org/software/gettext/"
-       DIE=1
-fi
-
-if test "$DIE" -eq 1; then
-	exit 1
-fi
-
-test $TEST_TYPE $FILE || {
+test -f lib/gftp.h || {
 	echo "You must run this script in the top-level $PROJECT directory"
 	exit 1
 }
 
-if test -z "$AUTOGEN_SUBDIR_MODE"; then
-        if test -z "$*"; then
-                echo "I am going to run ./configure with no arguments - if you wish "
-                echo "to pass any to it, please specify them on the $0 command line."
-        fi
-fi
+test -z "$AUTOMAKE"   && AUTOMAKE=automake
+test -z "$ACLOCAL"    && ACLOCAL=aclocal
+test -z "$AUTOCONF"   && AUTOCONF=autoconf
+test -z "$AUTOHEADER" && AUTOHEADER=autoheader
 
-case $CC in
-*xlc | *xlc\ * | *lcc | *lcc\ *) am_opt=--include-deps;;
-esac
+set -x 
 
-intl=`$GETTEXTIZE --help 2>/dev/null | grep -- '--intl'`
-if test -z "$intl"; then
-	GETTEXTIZE_FLAGS="-c"
-else
-	GETTEXTIZE_FLAGS="-c --intl -f"
-fi
+#Get all required m4 macros required for configure
+libtoolize --copy --force || exit 1
+$ACLOCAL -I m4 || exit 1
 
-echo "$GETTEXTIZE $GETTEXTIZE_FLAGS"
-$GETTEXTIZE $GETTEXTIZE_FLAGS
+#Generate config.h.in
+$AUTOHEADER --force || exit 1
 
-mv Makefile.am~ Makefile.am
-mv configure.in~ configure.in
+#Generate Makefile.in's
+touch config.rpath
+$AUTOMAKE --add-missing --copy --force || exit 1
+AUTOMAKE=$AUTOMAKE intltoolize -c --automake --force || exit 1
 
-ACLOCAL_FLAGS="-I m4"
-echo "$ACLOCAL $ACLOCAL_FLAGS"
-$ACLOCAL $ACLOCAL_FLAGS
+#generate configure
+$AUTOCONF --force || exit 1
 
-# optionally feature autoheader
-($AUTOHEADER --version)  < /dev/null > /dev/null 2>&1 && $AUTOHEADER
-
-AUTOMAKE_FLAGS="-a -c $am_opt"
-echo "$AUTOMAKE $AUTOMAKE_FLAGS"
-$AUTOMAKE $AUTOMAKE_FLAGS
-
-echo $AUTOCONF
-$AUTOCONF
-
-cd $ORIGDIR
-
-if test -z "$AUTOGEN_SUBDIR_MODE"; then
-        CFLAGS="-Wall -W -Wno-long-long -Wno-unused-parameter -ansi -D_GNU_SOURCE -O -g" $srcdir/configure "$@"
-
-        echo 
-        echo "Now type 'make' to compile $PROJECT."
-fi
