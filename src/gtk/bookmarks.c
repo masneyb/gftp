4aef2de471d5a93d7748f6ea74100554070d2e9d
wdlkmpx <wdlkmpx@gmail.com>
2020-01-28 06:57:38 +0800

    GtkComboBox Part 2: main window (replace GtkCombo)
    replace GtkCombo with GtkComboBoxText + text entry
    and try to keep the same behavior.
    
    A GtkComboBox works a bit differently..
    
    After many hours I managed to make it work properly.
    But launching gmrun (winkey + R) to run the screenshot app,
    after pressing enter, it also triggered a KEY_RELEASE signal in gFTP's combobox!..
    Apparently GtkComboBox does not receive KEY_PRESS events,
    I had to use the GtkEntry within (gtk_bin_get_child)..
    I fixed the issue by adding a variable that is
    set to 1 if signal = KEY_PRESS and 0 if signal = KEY_RELEASE
    
    problem: the toolbar looks bad and gtk_widget_set_size_request() does not set the height I specify.

--- src/gtk/bookmarks.c
+++ src/gtk/bookmarks.c
@@ -92,22 +92,22 @@ doadd_bookmark (gpointer * data, gftp_dialog_data * ddata)
     }
   *dpos = '\0';
 
-  edttxt = gtk_entry_get_text (GTK_ENTRY (GTK_COMBO (hostedit)->entry));
+  edttxt = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(hostedit));
   tempentry->hostname = g_strdup (edttxt);
 
-  edttxt = gtk_entry_get_text (GTK_ENTRY (GTK_COMBO (portedit)->entry));
+  edttxt = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(portedit));
   tempentry->port = strtol (edttxt, NULL, 10);
 
   proto = gftp_protocols[current_wdata->request->protonum].name;
   tempentry->protocol = g_strdup (proto);
 
-  edttxt = gtk_entry_get_text (GTK_ENTRY (GTK_COMBO (other_wdata->combo)->entry));
+  edttxt = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(other_wdata->combo));
   tempentry->local_dir = g_strdup (edttxt);
 
-  edttxt = gtk_entry_get_text (GTK_ENTRY (GTK_COMBO (current_wdata->combo)->entry));
+  edttxt = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(current_wdata->combo));
   tempentry->remote_dir = g_strdup (edttxt);
 
-  if ((edttxt = gtk_entry_get_text (GTK_ENTRY (GTK_COMBO (useredit)->entry))) != NULL)
+  if ( (edttxt = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(useredit))) != NULL)
     {
       tempentry->user = g_strdup (edttxt);
 
@@ -134,7 +134,7 @@ add_bookmark (gpointer data)
   if (!check_status (_("Add Bookmark"), current_wdata, 0, 0, 0, 1))
     return;
 
-  edttxt = gtk_entry_get_text (GTK_ENTRY (GTK_COMBO (hostedit)->entry));
+  edttxt = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(hostedit));
   if (*edttxt == '\0')
     {
       ftp_log (gftp_logging_error, NULL,
