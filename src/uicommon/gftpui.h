7b9c6680d676817c032ddfff8861fc76675a1d0c
wdlkmpx <wdlkmpx@gmail.com>
2021-04-16 20:37:10 +0800

    src/uicommon/gftpui.h: cosmetic changes

--- src/uicommon/gftpui.h
+++ src/uicommon/gftpui.h
@@ -65,13 +65,16 @@ typedef struct _gftpui_common_methods
   int (*subhelp_func) (const char *topic);
 } gftpui_common_methods;
 
+
 typedef struct _gftpui_common_curtrans_data
 {
   gftp_transfer * transfer;
   GList * curfle;
 } gftpui_common_curtrans_data;
 
+
 #define gftpui_common_use_threads(request)	(gftp_protocols[(request)->protonum].use_threads)
+
 
 #define GFTPUI_COMMON_COLOR_BLACK     "\033[30m"
 #define GFTPUI_COMMON_COLOR_RED       "\033[31m"
@@ -91,111 +94,96 @@ extern gftpui_common_methods gftpui_common_commands[];
 extern WGMutex gftpui_common_transfer_mutex;
 extern volatile sig_atomic_t gftpui_common_child_process_done;
 
+
+/* -------- */
 /* gftpui.c */
-int gftpui_run_callback_function	( gftpui_callback_data * cdata );
+/* -------- */
+int  gftpui_run_callback_function (gftpui_callback_data * cdata);
+int  gftpui_common_run_callback_function (gftpui_callback_data * cdata);
+void gftpui_common_init	 (int *argc, char ***argv, gftp_logging_func logfunc);
+void gftpui_common_about (gftp_logging_func logging_function, gpointer logdata);
 
-int gftpui_common_run_callback_function ( gftpui_callback_data * cdata );
+int gftpui_common_process_command (void *locui,
+                                   gftp_request * locreq,
+                                   void *remui,
+                                   gftp_request * remreq,
+                                   const char *command);
 
-void gftpui_common_init 		( int *argc,
-					  char ***argv,
-					  gftp_logging_func logfunc );
+int gftpui_common_cmd_open (void *uidata,
+                            gftp_request * request,
+                            void *other_uidata,
+                            gftp_request * other_request,
+                            const char *command);
 
-void gftpui_common_about 		( gftp_logging_func logging_function,
-					  gpointer logdata );
+int gftpui_common_cmd_mget_file (void *uidata,
+                                 gftp_request * request, 
+                                 void *other_uidata,
+                                 gftp_request * other_request,
+                                 const char *command);
 
-int gftpui_common_process_command 	( void *locui,
-					  gftp_request * locreq,
-					  void *remui,
-					  gftp_request * remreq,
-					  const char *command );
+int gftpui_common_cmd_mput_file (void *uidata,
+                                 gftp_request * request, 
+                                 void *other_uidata,
+                                 gftp_request * other_request,
+                                 const char *command);
 
-int gftpui_common_cmd_open 		( void *uidata,
-					  gftp_request * request,
-					  void *other_uidata,
-					  gftp_request * other_request,
-					  const char *command );
+gftp_transfer * gftpui_common_add_file_transfer (gftp_request * fromreq,
+                                                 gftp_request * toreq,
+                                                 void *fromuidata,
+                                                 void *touidata,
+                                                 GList * files);
 
-int gftpui_common_cmd_mget_file 	( void *uidata,
-					  gftp_request * request, 
-					  void *other_uidata,
-					  gftp_request * other_request,
-					  const char *command );
+void gftpui_cancel_file_transfer (gftp_transfer * tdata);
 
-int gftpui_common_cmd_mput_file 	( void *uidata,
-					  gftp_request * request, 
-					  void *other_uidata,
-					  gftp_request * other_request,
-					  const char *command );
+void gftpui_common_skip_file_transfer (gftp_transfer * tdata,
+                                       gftp_file * curfle);
 
-gftp_transfer * gftpui_common_add_file_transfer ( gftp_request * fromreq,
-						  gftp_request * toreq,
-						  void *fromuidata,
-						  void *touidata,
-						  GList * files );
+void gftpui_common_cancel_file_transfer	(gftp_transfer * tdata);
 
-void gftpui_cancel_file_transfer 	( gftp_transfer * tdata );
+int gftpui_common_transfer_files (gftp_transfer * tdata);
 
-void gftpui_common_skip_file_transfer	( gftp_transfer * tdata,
-					  gftp_file * curfle );
 
-void gftpui_common_cancel_file_transfer	( gftp_transfer * tdata );
-
-int gftpui_common_transfer_files 	( gftp_transfer * tdata );
-
+/* ---------------- */
 /* gftpuicallback.c */
-int gftpui_common_run_mkdir 		( gftpui_callback_data * cdata );
+/* ---------------- */
+int gftpui_common_run_mkdir   (gftpui_callback_data * cdata);
+int gftpui_common_run_rename  (gftpui_callback_data * cdata);
+int gftpui_common_run_site    (gftpui_callback_data * cdata);
+int gftpui_common_run_chdir   (gftpui_callback_data * cdata);
+int gftpui_common_run_chmod   (gftpui_callback_data * cdata);
+int gftpui_common_run_ls      (gftpui_callback_data * cdata);
+int gftpui_common_run_delete  (gftpui_callback_data * cdata);
+int gftpui_common_run_rmdir   (gftpui_callback_data * cdata);
+int gftpui_common_run_connect (gftpui_callback_data * cdata);
 
-int gftpui_common_run_rename 		( gftpui_callback_data * cdata );
 
-int gftpui_common_run_site 		( gftpui_callback_data * cdata );
-
-int gftpui_common_run_chdir 		( gftpui_callback_data * cdata );
-
-int gftpui_common_run_chmod		( gftpui_callback_data * cdata );
-
-int gftpui_common_run_ls 		( gftpui_callback_data * cdata );
-
-int gftpui_common_run_delete		( gftpui_callback_data * cdata );
-
-int gftpui_common_run_rmdir 		( gftpui_callback_data * cdata );
-
-int gftpui_common_run_connect 		( gftpui_callback_data * cdata );
-
+/* --------------------------------------------------------- */
 /* UI Functions that must be implemented by each distinct UI */
-void gftpui_lookup_file_colors 		( gftp_file * fle,
-					  char **start_color,
-					  char ** end_color );
+/* --------------------------------------------------------- */
+void gftpui_lookup_file_colors (gftp_file * fle,
+                                char ** start_color,
+                                char ** end_color);
 
-int gftpui_check_reconnect		( gftpui_callback_data * cdata );
+int gftpui_check_reconnect (gftpui_callback_data * cdata);
 
-void gftpui_refresh 			( void *uidata,
-					  int clear_cache_entry );
+void gftpui_refresh (void *uidata, int clear_cache_entry);
 
-void *gftpui_generic_thread 		( void *(*run_function)(void *data),
-					  void *data);
+void *gftpui_generic_thread (void *(*run_function)(void *data), void *data);
 
-void gftpui_show_busy           (gboolean busy);
+void gftpui_show_busy (gboolean busy);
 
-void gftpui_prompt_username		( void *uidata,
-					  gftp_request * request );
+void gftpui_prompt_username (void *uidata, gftp_request * request);
+void gftpui_prompt_password (void *uidata, gftp_request * request);
 
-void gftpui_prompt_password 		( void *uidata,
-					  gftp_request * request );
+void gftpui_add_file_to_transfer (gftp_transfer * tdata, GList * curfle);
+void gftpui_ask_transfer         (gftp_transfer * tdata);
 
-void gftpui_add_file_to_transfer 	( gftp_transfer * tdata,
-					  GList * curfle );
+void gftpui_start_current_file_in_transfer  (gftp_transfer * tdata);
+void gftpui_update_current_file_in_transfer (gftp_transfer * tdata);
+void gftpui_finish_current_file_in_transfer (gftp_transfer * tdata);
 
-void gftpui_ask_transfer		( gftp_transfer * tdata );
-
-void gftpui_start_current_file_in_transfer ( gftp_transfer * tdata );
-
-void gftpui_update_current_file_in_transfer ( gftp_transfer * tdata );
-
-void gftpui_finish_current_file_in_transfer ( gftp_transfer * tdata );
-
-void gftpui_start_transfer 		( gftp_transfer * tdata );
-
-void gftpui_disconnect 			( void *uidata );
+void gftpui_start_transfer (gftp_transfer * tdata);
+void gftpui_disconnect (void *uidata);
 
 
 #endif
