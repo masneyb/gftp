masneyb/gftp

commit 786b920c6d2e360befc3d7990a17f04fe49d1655

lib/..ftps.c: (implicit) ftpdat->auth_tls_start = ftpsi_auth_tls_start

lib/protocol_ftp.c
@@ -1900,7 +1900,6 @@ static void ftp_copy_param_options (gftp_request * dest_request,
   dftpdat->data_conn_read      = sftpdat->data_conn_read;
   dftpdat->data_conn_write     = sftpdat->data_conn_write;
   dftpdat->data_conn_tls_close = sftpdat->data_conn_tls_close;
-  dftpdat->implicit_ssl        = sftpdat->implicit_ssl;
   dftpdat->list_type           = sftpdat->list_type;
   dftpdat->last_cmd            = sftpdat->last_cmd;
   memcpy (dftpdat, sftpdat, sizeof(*sftpdat));

lib/protocol_ftps.c
@@ -24,6 +24,10 @@
 #include "gftp.h"
 #include "protocol_ftp.h"
 
+//=======================================================================
+//                          EXPLICIT TLS
+//=======================================================================
+
 #ifdef USE_SSL
 
 static int
@@ -88,18 +92,6 @@ static int ftps_auth_tls_start (gftp_request * request)
 
   ftpdat = request->protocol_data;
 
-  if (ftpdat->implicit_ssl) {
-      // buggy servers may require this (vsftpd)
-      // this is not needed for servers with proper implicit SSL support
-      //  (pure-FTPd, Rebex FTPS)
-      ret = ftp_send_command (request, "PBSZ 0\r\n", -1, 1, 0);
-      ret = ftp_send_command (request, "PROT P\r\n", -1, 1, 0);
-      return 0;
-  }
-
-  // explicit SSL
-  // send AUTH TLS and disconnect if it's not supported..
-
   ret = ftp_send_command (request, "AUTH TLS\r\n", -1, 1, 0);
   if (ret < 0)
     return (ret);
@@ -148,16 +140,9 @@ static int ftps_auth_tls_start (gftp_request * request)
 static int ftps_connect (gftp_request * request)
 {
   DEBUG_PRINT_FUNC
-  ftp_protocol_data * ftpdat = (ftp_protocol_data *) request->protocol_data;
-  if (request->datafd > 0) {
-    return (0);
-  }
-  if (!ftpdat->implicit_ssl) {
-     // explicit TLS
-     // need to send AUTH TLS in clear text
-     request->read_function  = gftp_fd_read;
-     request->write_function = gftp_fd_write;
-  }
+  // need to send AUTH TLS in clear text
+  request->read_function  = gftp_fd_read;
+  request->write_function = gftp_fd_write;
 
   return (ftp_connect (request));
 }
@@ -235,7 +220,23 @@ static int ftpsi_connect (gftp_request * request)
    if (!request->port) {
       request->port = gftp_protocols[GFTP_PROTOCOL_FTPSi].default_port;
    }
-   return (ftps_connect (request));
+   return (ftp_connect (request));
+}
+
+
+static int ftpsi_auth_tls_start (gftp_request * request)
+{
+   DEBUG_PRINT_FUNC
+   int ret;
+   // buggy servers may require this (vsftpd)
+   // this is not needed for servers with proper implicit SSL support
+   //  (pure-FTPd, Rebex FTPS)
+   ret = ftp_send_command (request, "PBSZ 0\r\n", -1, 1, 0);
+   ret = ftp_send_command (request, "PROT P\r\n", -1, 1, 0);
+   if (ret < 0) {
+      return (ret);
+   }
+   return 0;
 }
 
 
@@ -258,7 +259,6 @@ int ftpsi_init (gftp_request * request)
    request->url_prefix = gftp_protocols[GFTP_PROTOCOL_FTPSi].url_prefix;
 
    ftpdat = (ftp_protocol_data *) request->protocol_data;
-   ftpdat->implicit_ssl        = 1;
    ftpdat->data_conn_tls_start = ftps_data_conn_tls_start;
    ftpdat->data_conn_read      = gftp_ssl_read;
    ftpdat->data_conn_write     = gftp_ssl_write;
@@ -267,6 +267,7 @@ int ftpsi_init (gftp_request * request)
    request->write_function     = gftp_ssl_write;
 
    request->connect            = ftpsi_connect;
+   ftpdat->auth_tls_start      = ftpsi_auth_tls_start;
    request->post_connect       = gftp_ssl_session_setup; /* socket-connect.c */
 
    return 0;

lib/protocol_ftp.h
@@ -52,7 +52,6 @@ struct ftp_protocol_data_tag
   ssize_t (*data_conn_write) (gftp_request * request, const char *ptr,
                               size_t size, int fd);
   void (*data_conn_tls_close) (gftp_request * request);
-  unsigned int implicit_ssl : 1;
   int list_type;    /* LIST. See FTP_DIRTYPE_* above */
   int last_cmd;     /* 4hackz */
   unsigned int feat[FTP_FEAT_TOTAL];

commit e97993abbfe3d5cfe454422320a69a4cec4d806f

lib/...ftps.c: fix file transfers with TLS enabled
It was broken by 786b920c6d2e360befc3d7990a17f04fe49d1655

fix ftps_connect()

and ftpsi_init()

lib/protocol_ftp.c
@@ -174,6 +174,7 @@ static int ftp_read_response (gftp_request * request, int disconnect_on_42x)
            break;
         }
         // read text chunk, may not contain the whole response
+        DEBUG_TRACE("  request->read_function: %p\n", request->read_function)
         ret = request->read_function (request, tempstr, sizeof(tempstr), request->datafd);
         if (ret <= 0) {
            break;
@@ -246,6 +247,7 @@ static int ftp_read_response (gftp_request * request, int disconnect_on_42x)
   }
 
   if (!last_line) {
+     DEBUG_MSG("no last line!!!!\n\n")
      return (GFTP_ERETRYABLE);
   }
   DEBUG_TRACE("RESPONSE: %s\n", last_line);
@@ -294,10 +296,11 @@ int ftp_send_command (gftp_request * request, const char *command,
     command_len = strlen (command);
 
   DEBUG_TRACE("SEND_CMD: %s", command)
-  if ((ret = request->write_function (request, command, command_len, 
-                                      request->datafd)) < 0)
-    return (ret);
-
+  ret = request->write_function (request, command, command_len, request->datafd);
+  if (ret < 0) {
+     DEBUG_MSG("failed to write to socket!!!!\n\n");
+     return (ret);
+  }
   if (read_response)
     {
       ret = ftp_read_response (request, 1);
@@ -1335,6 +1338,8 @@ static off_t ftp_get_file (gftp_request * request, const char *filename,
   ftpdat = request->protocol_data;
   if (passive_transfer && ftpdat->feat[FTP_FEAT_PRET]) {
     ret = ftp_generate_and_send_command (request, "PRET RETR", filename,1, 0);
+    if (ret < 0)
+       return (ret);
   }
 
   ret = ftp_setup_file_transfer (request, filename, startsize, "RETR");
@@ -1722,7 +1727,7 @@ int ftp_get_next_file (gftp_request * request, gftp_file * fle, int fd)
       else
       {
          if (fle->file == NULL) {
-            DEBUG_MSG("@@ entry has been discarded\n");
+            DEBUG_MSG("@@ entry has been discarded\n")
             gftp_file_destroy (fle, 0);
             continue;
          }
@@ -1952,6 +1957,8 @@ void  ftp_register_module (void)
 {
   DEBUG_PRINT_FUNC
   gftp_register_config_vars (config_vars);
+  DEBUG_TRACE("~~  gftp_fd_read:  %p\n", gftp_fd_read)
+  DEBUG_TRACE("~~  gftp_fd_write: %p\n", gftp_fd_write)
 }
 
 

lib/protocol_ftps.c
@@ -140,6 +140,9 @@ static int ftps_auth_tls_start (gftp_request * request)
 static int ftps_connect (gftp_request * request)
 {
   DEBUG_PRINT_FUNC
+  if (request->datafd > 0) {
+      return (0);
+  }
   // need to send AUTH TLS in clear text
   request->read_function  = gftp_fd_read;
   request->write_function = gftp_fd_write;
@@ -150,8 +153,7 @@ static int ftps_connect (gftp_request * request)
 #endif
 
 
-void
-ftps_register_module (void)
+void ftps_register_module (void)
 {
   DEBUG_PRINT_FUNC
 #ifdef USE_SSL
@@ -215,6 +217,9 @@ void ftpsi_register_module (void)
 static int ftpsi_connect (gftp_request * request)
 {
    DEBUG_PRINT_FUNC
+   if (request->datafd > 0) {
+      return (0);
+   }
    // this is just to override default port
    // default port for FTPSi is 990..
    if (!request->port) {
@@ -266,6 +271,7 @@ int ftpsi_init (gftp_request * request)
    request->read_function      = gftp_ssl_read;
    request->write_function     = gftp_ssl_write;
 
+   request->init               = ftpsi_init;
    request->connect            = ftpsi_connect;
    ftpdat->auth_tls_start      = ftpsi_auth_tls_start;
    request->post_connect       = gftp_ssl_session_setup; /* socket-connect.c */

lib/sslcommon.c
@@ -68,6 +68,8 @@ void ssl_register_module (void)
     {
       gftp_register_config_vars (config_vars);
       module_registered = 1;
+      DEBUG_TRACE("~~   gftp_ssl_read:  %p\n", gftp_ssl_read)
+      DEBUG_TRACE("~~   gftp_ssl_write: %p\n", gftp_ssl_write)
     }
 }
 

src/uicommon/gftpui.c
@@ -1484,9 +1484,10 @@ _gftpui_common_trans_file_or_dir (gftp_transfer * tdata)
       if (curfle->size == 0)
         {
           curfle->size = gftp_get_file_size (tdata->fromreq, curfle->file);
-          if (curfle->size < 0)
+          if (curfle->size < 0) {
+            DEBUG_MSG("filesize < 0!!!!!!!!!!\n\n")
             return ((int) curfle->size);
-
+          }
           tdata->total_bytes += curfle->size;
         }
 
@@ -1494,8 +1495,10 @@ _gftpui_common_trans_file_or_dir (gftp_transfer * tdata)
         {
           curfle->transfer_action = GFTP_TRANS_ACTION_RESUME;
           curfle->startsize = gftp_get_file_size (tdata->toreq, curfle->destfile);
-          if (curfle->startsize < 0)
+          if (curfle->startsize < 0) {
+            DEBUG_MSG("(retry) filesize < 0!!!!!!!!!!\n\n")
             return ((int) curfle->startsize);
+          }
         }
 
       tdata->tot_file_trans = gftp_transfer_file (tdata->fromreq, curfle->file,